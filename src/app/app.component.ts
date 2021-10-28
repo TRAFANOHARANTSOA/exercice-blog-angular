51cb0683c313bd0518ae1b31a2ea20ffe49f5d7f
TRAFANOHARANTSOA <trafanoharantsoa@gmail.com>
2021-10-28 18:21:09 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'exercice-blog-angular';
+  // post: {
+  //   title: string,
+  //   cotent: string,
+  //   loveIts: number,
+  //   created_at: Date
+  // }
 }
