6ca0fd7d0d2b5943405e9bf52c181193a5d51764
TRAFANOHARANTSOA <trafanoharantsoa@gmail.com>
2021-11-17 16:47:37 +0100

    using interface & @input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from 'post';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  title = 'BLOGNAKA';
   
-  public mesPost:Post[] = [
-    {title:"Mon premier post", content:"Lorem ipsum",loveIts:0},
-    {title:"Mon deuxième post", content:"Lorem ipsum",loveIts:0},
-    {title:"Mon troisième post", content:"Lorem ipsum",loveIts:0}
-  ];
+  @Input() post: Post = {
+    title : 'Mon premier post',
+    content: 'Lorem ipsum',  
+    loveIts: 0 
+  }
+  constructor() {
+
+    
+  }
 
 }
 
-export interface Post {
-  title:string,
-  content:string,
-  loveIts:number,
-
-}
