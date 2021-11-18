c6b244275ae1dd81b2864e0d0652b521f7343256
TRAFANOHARANTSOA <trafanoharantsoa@gmail.com>
2021-11-18 11:05:23 +0100

    import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostLIstItemComponent } from './post-list-item/post-list-item.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostLIstItemComponent
+    PostLIstItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
