80364e7dc74c2e0fd0e0b0cd6686ec6d6a5b1e6d
TRAFANOHARANTSOA <trafanoharantsoa@gmail.com>
2021-11-23 16:13:03 +0100

    increment/decrement  likes and dislikes, change color when  < 0

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, InputDecorator, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list-item',
@@ -11,22 +11,25 @@ export class PostLIstItemComponent implements OnInit {
   @Input() content: string | undefined;
   @Input() loveIts: number | undefined;
   @Input() create_at: Date | undefined;
-    
+  
+  numberOfLoveIts: number = 0;
+  likeIts: number | undefined;
+  dontLikeIts: number | undefined;
+  
   constructor() {
   }
 
   ngOnInit() {
   }
 
-  getloveIts() {
-    return this.loveIts;
-  }
-
-  onLoveIts(){
-    console.log('j\'aime');
-  }
-
-  onDontLoveIts(){
-    console.log('je n\'aime pas');
+  addLoveIts() {
+  this.likeIts = ++ this.numberOfLoveIts;
+  console.log(this.numberOfLoveIts);
+  return this.likeIts;
+ } 
+  takeLoveIts(){
+    this.dontLikeIts = -- this.numberOfLoveIts;
+    console.log(this.numberOfLoveIts);
+    return this.dontLikeIts;
   }
 }
